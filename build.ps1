a7dd8a5bcde1195e3252d82ce17473ef8ddcfa2f
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2022-06-28 03:05:25 +0200

    complete the build.ps1 script

--- build.ps1
+++ build.ps1
@@ -119,3 +119,213 @@ Filter New-RCMerge {
     Catch { "ERROR: $($_.Exception.Message)" }
     Pop-Location
 }
+
+Filter Invoke-OnModuleBranch {
+    <#
+    .SYNOPSIS
+        Process a scriptblock on module branch
+    .NOTES
+        Precondition:
+        1. The current branch does not have unstaged changes.
+        2. The script block does not modify module
+    #>
+
+    Param([Parameter(Mandatory=$true)] $ScriptBlock)
+
+    Get-Module RegCli -ListAvailable |
+    ForEach-Object {
+        Push-Location $PSScriptRoot
+        git branch --show-current |
+        ForEach-Object {
+            Try {
+                git switch module --quiet 2> $Null
+                If (!$?) { Throw "StayOn_$_" }
+                git switch $_ --quiet 2> $Null
+                git stash push --include-untracked --quiet
+                git switch module --quiet
+                & $ScriptBlock
+            }
+            Catch { "ERROR: $($_.Exception.Message)" }
+            Finally {
+                git switch $_ --quiet 2> $Null
+                git stash pop --quiet > $Null 2>&1
+            }
+        }
+        Pop-Location
+    }
+}
+
+## TODO: Function must implement -WhatIf
+Filter Publish-RCModule {
+    <#
+    .SYNOPSIS
+        Publish module to PSGallery
+    .NOTES
+        Precondition:
+        1. The current branch is module
+        2. The NUGET_API_KEY environment variable is set.
+    #>
+
+    Invoke-OnModuleBranch {
+        If ((git branch --show-current) -ne 'module') { Throw 'BranchNotPwshModule' }
+        If ($null -eq $Env:NUGET_API_KEY) { Throw 'NUGET_API_KEY_IsNull' }
+        @{
+            Name = 'RegCli';
+            NuGetApiKey = $Env:NUGET_API_KEY;
+        } | ForEach-Object { Publish-Module @_ }
+        Write-Host "RegCli@v$((& $DevDependencies.Manifest).ModuleVersion) published"
+    }
+}
+
+## TODO: Function must implement -WhatIf
+Filter Push-RCModule {
+    <#
+    .SYNOPSIS
+        Push new module commit to GitHub
+    .NOTES
+        Precondition:
+        1. The current branch is module
+    .OUTPUTS
+        Push details
+    #>
+
+    Invoke-OnModuleBranch {
+        If ((git branch --show-current) -ne 'module') { Throw 'BranchNotPwhModule' }
+        git push origin module --force
+        If (!$?) { Throw 'PushModuleToGitHubFailed' }
+        "v$((& $DevDependencies.Manifest).ModuleVersion)" |
+        ForEach-Object {
+            If ($_ -inotin @(git tag --list)) {
+                git tag $_
+                git push --tags
+            }
+        }
+    }
+}
+
+Filter New-RCRelease {
+    <#
+    .SYNOPSIS
+        Create new module release on GitHub
+    .NOTES
+        Precondition:
+        1. The current branch is main
+        2. gh must be installed
+        3. Module version tag is listed on Github
+        4. The release tag is not listed on Github
+    #>
+
+    Param($Files)
+
+    Push-Location $PSScriptRoot
+    Try {
+        If ((git branch --show-current) -ne 'main') { Throw 'BranchNotMain' }
+        If ((where.exe gh.exe).Count -eq 0) { Throw 'GithubCliNotFoundOnPath' }
+        $Manifest = & $DevDependencies.Manifest
+        Switch ("v$($Manifest.ModuleVersion)") {
+            {$_ -inotin @(git ls-remote --tags origin |
+            ForEach-Object { ($_ -split '/')[-1] })} { Throw 'TagNotListedOnGithub' }
+            {$_ -in @(gh release list |
+            ConvertFrom-String | ForEach-Object { $_.P1 })} { Throw 'ReleaseTagExistsOnGitHub' }
+            Default {
+                gh release create $_ $Files --notes @"
+- [x] $(((Get-Content .\Readme.md -TotalCount 2 |
+Where-Object {$_ -like '*`[Test Coverage`]*'}) -split ' ',3)[2])
+$($Manifest.PrivateData.PSData.ReleaseNotes -split "`n" |
+ForEach-Object { "- [x] $_" } |
+Out-String)
+"@
+            }
+        }
+    }
+    Catch { "ERROR: $($_.Exception.Message)" }
+    Pop-Location
+}
+
+Filter Deploy-RCModule {
+    <#
+    .SYNOPSIS
+        Deploy module Everywhere
+    #>
+
+    Try {
+        { If ((git branch --show-current) -ne 'main') { Throw 'BranchNotMain' } } |
+        ForEach-Object {
+            & $_
+            New-RCMerge
+            & $_
+        }
+        Push-RCModule
+        New-RCRelease
+        Publish-RCModule
+    }
+    Catch { "ERROR: $($_.Exception.Message)" }
+}
+
+Function Update-RCHelp {
+    <#
+    .SYNOPSIS
+        Update RegCli help document
+    .NOTES
+        Precondition: PlatyPS is installed
+    #>
+
+    Begin {
+        $PlatyPsModule = Get-Module | Where-Object Name -eq 'PlatyPS'
+        Import-Module PlatyPS -RequiredVersion $DevDependencies.PlatyPS -Force
+    }
+    Process {
+        Push-Location $PSScriptRoot
+        New-ExternalHelp -Path .\en_us\ -OutputPath en-US -Force
+        Pop-Location
+    }
+    End {
+        Remove-Module PlatyPS -Force
+        If ($PlatyPsModule.Count -gt 0) {
+            Import-Module PlatyPS -RequiredVersion $PlatyPsModule.Version -Force
+        }
+    }
+}
+
+Filter Install-BuildDependencies {
+    <#
+    .SYNOPSIS
+        Install build modules
+    #>
+
+    {
+        Param(
+            $Name,
+            $PreInstall
+        )
+        
+        If ((Get-Module $Name -ListAvailable |
+        Where-Object Version -eq $DevDependencies[$Name]).Count -eq 0) {
+            If ($Null -ne $PreInstall) { & $PreInstall }
+            Install-Module $Name -RequiredVersion $DevDependencies[$Name] -Force
+        }
+    } | ForEach-Object { & $_ PlatyPS }
+}
+
+Filter New-RCJunction {
+    <#
+    .SYNOPSIS
+        Create the RegCli Junction in PSModulePath
+    #>
+
+    Param([switch] $Force)
+
+    $FirstPath = "$(($env:PSModulePath -split ';')[0])\RegCli"
+    If (Test-Path $FirstPath) {
+        If ($Force) {
+            Remove-Item $FirstPath -Force
+        } Else {
+            Return 'RegCli junction already exists'
+        }
+    }
+    @{
+        Path = $FirstPath;
+        ItemType = 'Junction';
+        Value = $PSScriptRoot
+    } | ForEach-Object { New-Item @_ }
+}
