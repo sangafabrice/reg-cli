f9677decdebd861e695571f26b7dc7e8de054e39
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2022-06-28 02:36:12 +0200

    add new-rcmerge

--- build.ps1
+++ build.ps1
@@ -53,3 +53,69 @@ Filter New-RCManifest {
     }
     Pop-Location
 }
+
+Filter New-RCMerge {
+    <#
+    .SYNOPSIS
+        Merge module into main
+    .NOTES
+        Precondition:
+        1. The current branch is main
+        2. Module files are modified in main
+        3. The module manifest is modified
+    #>
+
+    Param($CommitMessage)
+
+    Push-Location $PSScriptRoot
+    Try {
+        If ((git branch --show-current) -ne 'main') { Throw 'BranchNotMain' }
+        New-RCManifest
+        $Manifest = & $DevDependencies.Manifest
+        $FileList = $Manifest.FileList
+        $ManifestFile = $FileList | Where-Object {$_ -like '*.psd1'}
+        Test-ModuleManifest $ManifestFile
+        If ($Null -eq $CommitMessage) {
+            $CommitMessage = Switch ($Manifest.PrivateData.PSData.ReleaseNotes) {
+                { ($_ -split "`n").Count -eq 1 } { "$_" }
+                Default { "RELEASE: v$($Manifest.ModuleVersion)" }
+            }
+        }
+        $GitDiffFiles = @(git diff --name-only --cached) + @(git diff --name-only)
+        If ($GitDiffFiles.Count -eq 0) { Throw }
+        ,($GitDiffFiles | Select-Object -Unique) |
+        ForEach-Object {
+            If ($_.Count -eq $_.Where({$_ -in $FileList}, 'Until').Count) { Throw 'ModuleFilesNotModified' }
+            If ($Null -eq ($_ | Where-Object { $_ -eq $ManifestFile })) { Throw 'ModuleManifestNotModified' }
+        }
+        Invoke-Expression "git add $($FileList) latest.json Readme.md"
+        git commit --message "$CommitMessage" --quiet
+        git stash push --include-untracked --quiet
+        git switch module --quiet 2> $Null
+        If (!$?) {
+            git stash pop --quiet > $Null 2>&1 
+            Throw
+        }
+        git merge --no-commit main > $Null 2>&1 
+        $IsMergeError = !$?
+        $CDPattern = "$($PWD -replace '\\','\\')\\"
+        Get-ChildItem -Recurse -File |
+        Where-Object { ($_.FullName -replace $CDPattern) -inotin $FileList } |
+        Remove-Item
+        Get-ChildItem -Directory |
+        Where-Object { ($_.FullName -replace $CDPattern) -inotin @($FileList |
+            Where-Object { $_ -like '*\*' } |
+            ForEach-Object { ($_ -split '\\')[0] }) } |
+        Remove-Item -Recurse -Force
+        If ($IsMergeError) {
+            ,@(git diff --name-only) |
+            ForEach-Object { If ($_ -in $FileList) { Throw 'MergeConflict' } }
+            git add .
+        }
+        git commit --message "$CommitMessage" --quiet
+        git switch main --quiet 2> $Null
+        git stash pop --quiet > $Null 2>&1
+    }
+    Catch { "ERROR: $($_.Exception.Message)" }
+    Pop-Location
+}
