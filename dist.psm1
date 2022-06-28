e68bb803788b77ec936b08ab96a933f8b6b25f58
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2022-06-28 12:29:11 +0200

    Initial deployment script

---
+++ dist.psm1
@@ -0,0 +1,65 @@
+$DevDependencies = @{
+    ProgramName = '_'
+    RemoteRepo = (git ls-remote --get-url) -replace '\.git$'
+}
+
+Function New-UpdaterScript {
+    <#
+    .SYNOPSIS
+        Create module manifest
+    .NOTES
+        Precondition:
+        1. latest.json exists
+    #>
+
+    $GithubRepo = $DevDependencies.RemoteRepo
+    $HeaderPath = '.\Header.ps1'
+    Push-Location $PSScriptRoot
+    Get-Content .\latest.json -Raw |
+    ConvertFrom-Json |
+    ForEach-Object {
+        @{
+            Path = $HeaderPath
+            Version = $_.version
+            GUID = '0f0234b8-2357-4909-a0b2-094a02e96be4'
+            Author = 'Fabrice Sanga'
+            CompanyName = 'sangafabrice'
+            Copyright = "© $((Get-Date).Year) SangaFabrice. All rights reserved."
+            Description = 'Description'
+            RequiredModules = @('DownloadInfo','RegCli')
+            ExternalModuleDependencies = @('DownloadInfo','RegCli')
+            Tags = @('Tag')
+            LicenseUri = "$GithubRepo/blob/main/LICENSE.md"
+            ProjectUri = "$GithubRepo/tree/$(git branch --show-current)"
+            IconUri = ''
+            ReleaseNotes = $_.releaseNotes
+        }
+    } | ForEach-Object { New-ScriptFileInfo @_ -Force }
+    ((Get-Content $HeaderPath).Where({$_ -like 'Param()*'}, 'Until') +
+    (Get-Content .\Update.ps1)) -join "`n" | Out-String
+    Remove-Item $HeaderPath
+    Pop-Location
+}
+
+Function Publish-UpdaterScript {
+    <#
+    .SYNOPSIS
+        Publish script to PSGallery
+    .NOTES
+        Precondition:
+        1. The NUGET_API_KEY environment variable is set.
+    #>
+
+    @{
+        Path = "$PSScriptRoot\Update$($DevDependencies.ProgramName).ps1" |
+            ForEach-Object {
+                New-UpdaterScript | Out-File $_
+                Return $_
+            }
+        NuGetApiKey = $Env:NUGET_API_KEY
+    } | ForEach-Object { 
+        Publish-Script @_
+        Write-Host "$((Get-Item $_.Path).Name) published"
+        Remove-Item $_.Path -Exclude Update.ps1
+    }
+}
