84a26f8eae14a7b7fdaae03759db7ecda9e0fd51
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2022-07-05 18:27:26 +0200

    add New-RegCliUpdate functions

--- Update.ps1
+++ Update.ps1
@@ -1,3 +1,4 @@
+[CmdletBinding()]
 Param (
     [ValidateNotNullOrEmpty()]
     [ValidateScript({
@@ -25,39 +26,103 @@ Param (
     $SaveTo = $PSScriptRoot
 )
 
-Switch (
-    Get-DownloadInfo -PropertyList @{
-        RepositoryId = 'ytdl-org/youtube-dl'
-        AssetPattern = 'youtube-dl.exe$|SHA2-512SUMS$'
-    }
-) {
-    {
-        @($_.Version,$_.Link) |
-        ForEach-Object { $_ -notin @($Null, '') }
-    } {
-        $SaveToContent = Get-Item "$SaveTo\*"
-        $Version = [version]$_.Version
-        $Installer = $SaveToContent.Where({ [version]$_.VersionInfo.ProductVersion -eq $Version }).FullName ??
-            "$SaveTo\$($_.Version).exe"
-        $Checksum = (((Invoke-WebRequest "$($_.Link.Where({$_.Url -like '*512SUMS'}).Url)").Content |
-            ForEach-Object { [char] $_ }) -join '' -split "`n" |
-            ConvertFrom-String).Where({$_.P2 -ieq 'youtube-dl.exe'}).P1
-        If (!(Test-Path $Installer)) {
-            Save-Installer "$($_.Link.Where({$_.Url -like '*.exe'}).Url)" |
-            ForEach-Object { 
-                If ($Checksum -ieq (Get-FileHash $_ SHA512).Hash) {
-                    $SaveToContent.Where({ $_.VersionInfo.FileDescription -ieq 'YouTube video downloader' }) |
-                    Remove-Item
-                    Move-Item $_ -Destination $Installer
-                } 
-            }
+& {
+    $VerbosePreferenceBool = $VerbosePreference -ine 'SilentlyContinue'
+    Write-Verbose 'Retrieve install or update information...'
+    $UpdateInfo = 
+        Get-DownloadInfo -PropertyList @{
+            RepositoryId = 'ytdl-org/youtube-dl'
+            AssetPattern = 'youtube-dl.exe$|SHA2-512SUMS$'
+        } |
+        Where-Object {
+            @($_.Version,$_.Link) |
+            ForEach-Object { $_ -notin @($Null, '') }
         }
-        @{
-            Path = $InstallLocation
-            ItemType = 'SymbolicLink'
-            Value = $Installer
-            ErrorAction = 'SilentlyContinue'
-            Force = $True
-        } | ForEach-Object { New-Item @_ }
+    $InstallerVersion = $UpdateInfo.Version
+    $InstallerDescription = 'YouTube video downloader'
+    If ($UpdateInfo.Count -le 0) {
+        $InstallerVersion = "$(
+            Get-ChildItem $SaveTo |
+            Select-Object VersionInfo -ExpandProperty VersionInfo |
+            Where-Object { $_.FileDescription -ieq $InstallerDescription } |
+            ForEach-Object { [version] $_.ProductVersion } |
+            Sort-Object -Descending |
+            Select-Object -First 1
+        )"
     }
+    Try {
+        New-RegCliUpdate $InstallLocation $SaveTo $InstallerVersion $InstallerDescription |
+        Import-Module -Verbose:$False -Force
+        If ($UpdateInfo.Count -gt 0) {
+            Start-InstallerDownload "$($UpdateInfo.Link.Where({ $_.Url -like '*.exe' }).Url)" "$(
+                (((Invoke-WebRequest "$($UpdateInfo.Link.Where({$_.Url -like '*512SUMS'}).Url)" -Verbose:$False).Content |
+                ForEach-Object { [char] $_ }) -join '' -split "`n" |
+                ConvertFrom-String).Where({$_.P2 -ieq 'youtube-dl.exe'}).P1
+            )" -Verbose:$VerbosePreferenceBool
+        }
+        Remove-InstallerOutdated -Verbose:$VerbosePreferenceBool
+        $TestInstall = $False
+        $InstallDirectory = $InstallLocation -replace '(\\|/)[^\\/]+$'
+        If ([string]::IsNullOrEmpty($InstallDirectory)) { $InstallDirectory = $PWD }
+        If ("$((Get-Item $InstallDirectory -ErrorAction SilentlyContinue).FullName)" -ieq (Get-Item $SaveTo).FullName) {
+            # If $InstallLocation directory is equal to $SaveTo
+            @{
+                NewName = & {
+                    [void] ($InstallLocation -match '(?<ExeName>[^\\/]+$)')
+                    $Matches.ExeName
+                }
+                LiteralPath = Get-InstallerPath
+                ErrorAction = 'SilentlyContinue'
+                Force = $True
+            } | ForEach-Object { Rename-Item @_ }
+            $TestInstall = Test-Path $InstallLocation
+        } Else {
+            New-Item $InstallDirectory -ItemType Directory -Force | Out-Null
+            @{
+                Path = $InstallLocation
+                ItemType = 'SymbolicLink'
+                Value = Get-InstallerPath
+                ErrorAction = 'SilentlyContinue'
+                Force = $True
+            } | ForEach-Object { New-Item @_ | Out-Null }
+            $TestInstall = (Get-Item (Get-Item $InstallLocation).Target).FullName -ieq (Get-Item (Get-InstallerPath)).FullName
+        }
+        If ($TestInstall) { Write-Verbose "$InstallerDescription $InstallerVersion installation complete." }
+    } 
+    Catch { }
 }
+
+<#
+.SYNOPSIS
+    Updates Youtube video downloader command line tool.
+.DESCRIPTION
+    The script installs or updates Youtube video downloader command line tool on Windows.
+.NOTES
+    Required: at least Powershell Core 7.
+.PARAMETER InstallLocation
+    Path to the console app.
+    It is restricted to file system paths.
+    It does not necessary exists.
+    It defaults to %LOCALAPPDATA%\Microsoft\WindowsApps\youtube-dl.exe.
+.PARAMETER SaveTo
+    Path to the directory of the downloaded installer.
+    It is an existing file system path.
+    It defaults to the script directory.
+.EXAMPLE
+    Get-ChildItem C:\ProgramData\YoutubeDl -ErrorAction SilentlyContinue
+
+    PS > .\UpdateYoutubeDl.ps1 -InstallLocation C:\ProgramData\YoutubeDl\youtube-dl.exe -SaveTo .
+
+    PS > Get-ChildItem C:\ProgramData\YoutubeDl | Select-Object Name
+    Name
+    ----
+    youtube-dl.exe
+
+    PS > Get-ChildItem | Select-Object Name
+    Name
+    ----
+    2021.12.17.exe
+    UpdateYoutubeDl.ps1
+
+    Install Youtube video downloader to 'C:\ProgramData\YoutubeDl' and save its setup installer to the current directory.
+#>
