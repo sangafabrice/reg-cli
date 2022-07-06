6e11be33b3d9179f10a53e66d2a29e3bae77dd62
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2022-07-06 19:01:39 +0200

    Fix: Symlink target does not copy to ExecutablePath.

--- RegCli.psm1
+++ RegCli.psm1
@@ -161,8 +161,11 @@ Class RegCli {
     Static [MachineType] GetExeMachineType([string] $ExecutablePath) {
         # Get the machine type of an application
 
-        (Get-Item $ExecutablePath -ErrorAction SilentlyContinue).Where({ $_.LinkType -ieq 'SymbolicLink' }) |
-        ForEach-Object { $Script:ExecutablePath = $_.LinkTarget }
+        Switch (
+            (Get-Item $ExecutablePath -ErrorAction SilentlyContinue).
+            Where({ $_.LinkType -ieq 'SymbolicLink' }) |
+            ForEach-Object { $_.LinkTarget }
+        ) { Default { $ExecutablePath = $_ } }
         Switch ($(Try { (Get-Item -LiteralPath $ExecutablePath).FullName } Catch { })) {
             { ![string]::IsNullOrEmpty($_) } {
                 $PEHeaderOffset = [Byte[]]::New(2)
