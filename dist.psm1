fa58a7c4c4acef39afa49bfe3d04ba192cd16971
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2022-07-09 18:16:02 +0200

    change icon

--- dist.psm1
+++ dist.psm1
@@ -2,7 +2,7 @@ $DevDependencies = @{
     ProgramName = 'Blisk'
     Description = 'The script installs or updates Blisk browser on Windows.'
     Guid = '0f0234b8-2357-4909-a0b2-094a02e96be4'
-    IconUri = 'https://rawcdn.githack.com/sangafabrice/reg-cli/88e8345bfd93c03273280405759b79dae697a7f0/icon.png'
+    IconUri = 'https://rawcdn.githack.com/sangafabrice/reg-cli/58d15da2c7da327f3561e3c86884582d7e0db854/icon.png'
     Tags = @('blisk','chromium','update','browser')
     RemoteRepo = (git ls-remote --get-url) -replace '\.git$'
 }
