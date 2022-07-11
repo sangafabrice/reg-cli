363ae3d7fb7b37a4bd4f523465361d000b8fc831
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2022-07-11 12:47:27 +0200

    initialize msedge updater

--- Update.ps1
+++ Update.ps1
@@ -17,7 +17,7 @@ Param (
             } | ForEach-Object { Get-Item @_ }
         ).FullName -ine $PSScriptRoot
     })] [string]
-    $InstallLocation = "${Env:ProgramData}\Blisk",
+    $InstallLocation = "${Env:ProgramData}\MSEdge",
     [ValidateNotNullOrEmpty()]
     [ValidateScript({
         (Get-Item -LiteralPath $_).PSDrive.Name -iin 
@@ -27,39 +27,59 @@ Param (
 )
 
 & {
-    $NameLocation = "$InstallLocation\blisk.exe"
+    $BaseNameLocation = "$InstallLocation\msedge"
+    $NameLocation = "$BaseNameLocation.exe"
     $VerbosePreferenceBool = $VerbosePreference -ine 'SilentlyContinue'
     Write-Verbose 'Retrieve install or update information...'
     $UpdateInfo = 
         $(Try {
-            @{
-                Uri = 'https://blisk.io/download/?os=win'
-                UserAgent = 'NSISDL/1.2 (Mozilla)'
-                MaximumRedirection = 0
-                SkipHttpErrorCheck = $True
-                ErrorAction = 'SilentlyContinue'
-                Verbose = $False
-            } | ForEach-Object { (Invoke-WebRequest @_).Headers.Location } |
-            Select-Object @{
-                Name = 'Version'
-                Expression = {
-                    [void] ($_ -match "BliskInstaller_(?<Version>(\d+\.){3}\d+)\.exe$")
-                    [version] $Matches.Version
+            $UriBasis = "https://msedge.api.cdp.microsoft.com/api/v1.1/contents/Browser/namespaces/Default/names/msedge-stable-win-$(Get-ExecutableType $NameLocation)/versions/"
+            $WebRequestArgs = {
+                Param($ActionString)
+                Return @{
+                    Uri = "$UriBasis$ActionString"
+                    UserAgent = 'winhttp'
+                    Method = 'POST'
+                    Body = '{"targetingAttributes":{}}'
+                    Headers = @{ 'Content-Type' = 'application/json' }
+                    Verbose = $False
                 }
-            },@{
-                Name = 'Link'
-                Expression = { $_ }
-            } -Unique |
-            Where-Object { ![string]::IsNullOrEmpty($_.Version) } |
-            Sort-Object -Descending -Property Version |
-            Select-Object -First 1
+            }
+            & $WebRequestArgs 'latest?action=select' |
+            ForEach-Object { 
+                Invoke-WebRequest @_ |
+                ConvertFrom-Json |
+                ForEach-Object {
+                    $Version = $_.ContentId.Version
+                    & $WebRequestArgs "$Version/files?action=GenerateDownloadInfo" |
+                    ForEach-Object { 
+                        (Invoke-WebRequest @_).Content |
+                        ConvertFrom-Json |
+                        Select-Object @{
+                            Name = 'Size'
+                            Expression = { $_.SizeInBytes }
+                        },@{
+                            Name = 'Version'
+                            Expression = { $Version }
+                        },@{
+                            Name = 'Name'
+                            Expression = { $_.FileId }
+                        },@{
+                            Name = 'Link'
+                            Expression = { $_.Url }
+                        } |
+                        Sort-Object -Property Size -Descending |
+                        Select-Object Version,Link,Name -First 1
+                    }
+                }
+            }
         } Catch { }) |
         Where-Object {
-            @($_.Version,$_.Link) |
+            @($_.Version,$_.Link,$_.Name) |
             ForEach-Object { $_ -notin @($Null, '') }
         }
     $InstallerVersion = $UpdateInfo.Version
-    $SoftwareName = 'Blisk'
+    $SoftwareName = 'Microsoft Edge'
     $InstallerDescription = "$SoftwareName Installer"
     If ($UpdateInfo.Count -le 0) {
         $InstallerVersion = "$(
@@ -74,15 +94,26 @@ Param (
     Try {
         New-RegCliUpdate $NameLocation $SaveTo $InstallerVersion $InstallerDescription |
         Import-Module -Verbose:$False -Force
-        If ($UpdateInfo.Count -gt 0) { Start-InstallerDownload "$($UpdateInfo.Link)" -Verbose:$VerbosePreferenceBool }
+        If ($UpdateInfo.Count -gt 0) {
+            Start-InstallerDownload $UpdateInfo.Link -Name $UpdateInfo.Name -Force -Verbose:$VerbosePreferenceBool
+        }
         Remove-InstallerOutdated -Verbose:$VerbosePreferenceBool
         If (Test-InstallOutdated) {
             Write-Verbose 'Current install is outdated or not installed...'
             Expand-ChromiumInstaller (Get-InstallerPath) $NameLocation
         }
-        Set-ChromiumVisualElementsManifest "$InstallLocation\chrome.VisualElementsManifest.xml" '#5F6368'
+        Set-ChromiumVisualElementsManifest "$BaseNameLocation.VisualElementsManifest.xml" '#173A73'
         Set-ChromiumShortcut $NameLocation
-        Set-BatchRedirect 'blisk' $NameLocation
+        Edit-TaskbarShortcut $NameLocation
+        Set-BatchRedirect 'msedge' $NameLocation
+        #Region: Set shell verb to open a PDF file as an MSEdge app
+        @{
+            Path = 'Registry::HKEY_CLASSES_ROOT\MSEdgePDF\shell\open\command'
+            Name = '(default)'
+            Value = '"' + $NameLocation + '" --app="%1"'
+            Force = $True
+        } | ForEach-Object { Set-ItemProperty @_ }
+        #EndRegion
         If (!(Test-InstallOutdated)) { Write-Verbose "$SoftwareName $InstallerVersion installation complete." }
     } 
     Catch { }
@@ -90,38 +121,40 @@ Param (
 
 <#
 .SYNOPSIS
-    Updates Blisk browser software.
+    Updates Microsoft Edge browser software.
 .DESCRIPTION
-    The script installs or updates Blisk browser on Windows.
+    The script installs or updates Microsoft Edge browser on Windows.
 .NOTES
     Required: at least Powershell Core 7.
 .PARAMETER InstallLocation
     Path to the installation directory.
     It is restricted to file system paths.
     It does not necessary exists.
-    It defaults to %ProgramData%\Blisk.
+    It defaults to %ProgramData%\MSEdge.
 .PARAMETER SaveTo
     Path to the directory of the downloaded installer.
     It is an existing file system path.
     It defaults to the script directory.
 .EXAMPLE
-    Get-ChildItem C:\ProgramData\Blisk -ErrorAction SilentlyContinue
+    Get-ChildItem C:\ProgramData\MSEdge -ErrorAction SilentlyContinue
 
-    PS > .\UpdateBlisk.ps1 -InstallLocation C:\ProgramData\Blisk -SaveTo .
+    PS > .\UpdateMSEdge.ps1 -InstallLocation C:\ProgramData\MSEdge -SaveTo .
 
-    PS > Get-ChildItem C:\ProgramData\Blisk | Select-Object Name
+    PS > Get-ChildItem C:\ProgramData\MSEdge | Select-Object Name -First 5
     Name
     ----
-    18.0.193.167
-    blisk.exe
-    chrome.VisualElementsManifest.xml
-    chrome_proxy.exe
+    BHO
+    EBWebView
+    edge_feedback
+    Extensions
+    identity_proxy
+    Locales
 
     PS > Get-ChildItem | Select-Object Name
     Name
     ----
-    18.0.193.167.exe
-    UpdateBlisk.ps1
+    103.0.1264.49.exe
+    UpdateMSEdge.ps1
 
-    Install Blisk browser to 'C:\ProgramData\Blisk' and save its setup installer to the current directory.
+    Install MSEdge browser to 'C:\ProgramData\MSEdge' and save its setup installer to the current directory.
 #>
--- dist.psm1
+++ dist.psm1
@@ -1,9 +1,9 @@
 $DevDependencies = @{
-    ProgramName = 'Blisk'
-    Description = 'The script installs or updates Blisk browser on Windows.'
-    Guid = '0f0234b8-2357-4909-a0b2-094a02e96be4'
+    ProgramName = 'MSEdge'
+    Description = 'The script installs or updates Microsoft Edge browser on Windows.'
+    Guid = '711fbaea-637f-4424-99e6-0a3c06e62874'
     IconUri = 'https://rawcdn.githack.com/sangafabrice/reg-cli/58d15da2c7da327f3561e3c86884582d7e0db854/icon.png'
-    Tags = @('blisk','chromium','update','browser')
+    Tags = @('edge','microsoft','chromium','update','browser')
     RemoteRepo = (git ls-remote --get-url) -replace '\.git$'
 }
 
