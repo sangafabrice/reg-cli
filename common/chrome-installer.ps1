518f8e902fd06689703f996226d1aef858049794
Fabrice Sanga <69244030+sangafabrice@users.noreply.github.com>
2022-08-11 20:12:10 +0200

    modify chrome-installer

--- common/chrome-installer.ps1
+++ common/chrome-installer.ps1
@@ -7,6 +7,7 @@ param (
     [string] $SoftwareName,
     [string] $InstallerDescription,
     [string] $BatchRedirectName,
+    [ValidateScript({ ForEach ($key in @('BaseNameLocation','HexColor')) { $_.ContainsKey($key) } })]
     [hashtable] $VisualElementManifest,
     [switch] $SkipSslValidation
 )
@@ -16,15 +17,20 @@ param (
     $InstallerVersion = [version] $UpdateInfo.Version
     If (!$UpdateInfo) { $InstallerVersion = Get-SavedInstallerVersion $SaveTo $InstallerDescription }
     Try {
-        $UpdateModule =
-            New-RegCliUpdate $NameLocation $SaveTo $InstallerVersion $InstallerDescription |
-            Import-Module -Verbose:$False -Force -PassThru
-        $UpdateInfo | Start-InstallerDownload -Verbose:$IsVerbose -Force:$SkipSslValidation
-        Remove-InstallerOutdated -Verbose:$IsVerbose
-        Expand-ChromiumInstaller (Get-InstallerPath) $NameLocation -Verbose:$IsVerbose
-        Set-ChromiumVisualElementsManifest "$($VisualElementManifest.Name).VisualElementsManifest.xml" $VisualElementManifest.RGBColor
-        Set-ChromiumShortcut $NameLocation
-        Set-BatchRedirect $BatchRedirectName $NameLocation
+        Switch ($NameLocation) {
+            Default {
+                $UpdateModule =
+                    New-RegCliUpdate $_ $SaveTo $InstallerVersion $InstallerDescription |
+                    Import-Module -Verbose:$False -Force -PassThru
+                $UpdateInfo | Start-InstallerDownload -Verbose:$IsVerbose -Force:$SkipSslValidation
+                Remove-InstallerOutdated -Verbose:$IsVerbose
+                Expand-ChromiumInstaller (Get-InstallerPath) $_ -Verbose:$IsVerbose
+                $VisualElementManifest |
+                ForEach-Object { Set-ChromiumVisualElementsManifest "$($_.BaseNameLocation).VisualElementsManifest.xml" $_.HexColor }
+                Set-ChromiumShortcut $_
+                Set-BatchRedirect $BatchRedirectName $_
+            }
+        }
         If (!(Test-InstallOutdated)) { Write-Verbose "$SoftwareName $(Get-ExecutableVersion) installation complete." }
     } 
     Finally { Remove-Module $UpdateModule -Verbose:$False }
