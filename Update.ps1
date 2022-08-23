62138344689ed63f3c776a6c22b8eb85bd4723ca
Fabrice Sanga <69244030+sangafabrice@users.noreply.github.com>
2022-08-23 15:55:45 +0200

    initial tor updater script.

--- Update.ps1
+++ Update.ps1
@@ -12,59 +12,33 @@ Param (
 
 & {
     $NameLocation = "$InstallLocation\firefox.exe"
-    $VerbosePreferenceBool = $VerbosePreference -ine 'SilentlyContinue'
-    Write-Verbose 'Retrieve install or update information...'
-    $UpdateInfo = $(
-        Try {
-            $UriBase = 'https://dist.torproject.org/torbrowser'
-            (Invoke-WebRequest $UriBase -Verbose:$False).Links.href |
-            Where-Object { $_ -match '(\d+\.)+\d+/$' } |
-            Select-Object @{Name = 'Version'; Expression = { [version] ($_ -replace '/$') }},@{Name = 'Path'; Expression = { $_ }} |
-            Sort-Object -Descending -Property Version|
-            Select-Object @{
-                Name = 'Link'
-                Expression = {
-                    $UriBase = "$UriBase/$($_.Path)"
-                    $Culture = Get-Culture
-                    $Lang = $Culture.TwoLetterISOLanguageName
-                    $OSArch = $(Switch (Get-ExecutableType $NameLocation) { 'x64' { 'win64-' } 'x86' { '' } })
-                    $LangInstallers = (Invoke-WebRequest $UriBase -Verbose:$False).Links.href
-                    $GroupInstaller = $LangInstallers | Where-Object { $_ -like "torbrowser-install-$OSArch*_$Lang*.exe" }
-                    Switch ($GroupInstaller.Count) {
-                        0 { $GroupInstaller = $LangInstallers | Where-Object { $_ -like "torbrowser-install-$OSArch*_en-US.exe" } }
-                        { $_ -gt 1 } {
-                            [void] ($Culture.Name -match '\-(?<Country>[A-Z]{2})$')
-                            $TempLine = $GroupInstaller | Where-Object { $_ -like "torbrowser-install-$OSArch*_$Lang-$($Matches.Country).exe" }
-                            If ([string]::IsNullOrEmpty($TempLine)) {
-                                If ($Lang -ieq 'en') { $TempLine = $GroupInstaller | Where-Object { $_ -like "torbrowser-install-$OSArch*_en-US.exe" } }
-                                Else { $TempLine = $GroupInstaller[0] }
-                            }
-                            $GroupInstaller = $TempLine
-                        }
-                    }
-                    Return "$UriBase$GroupInstaller"
-                }
-            } -First 1 | Select-Object Link,@{
-                Name = 'Version'
-                Expression = { [datetime] "$((Invoke-WebRequest $_.Link -Method Head -Verbose:$False).Headers.'Last-Modified')" }
-            }
-        }
-        Catch { }
-    ) | Select-NonEmptyObject
-    $InstallerVersion = $UpdateInfo.Version
-    $InstallerDescription = 'CN="The Tor Project, Inc."'
-    If (!$UpdateInfo) { $InstallerVersion = Get-SavedInstallerLastModified $SaveTo $InstallerDescription -UseSignature }
     Try {
-        New-RegCliUpdate $NameLocation $SaveTo $InstallerVersion $InstallerDescription -UseSignature |
-        Import-Module -Verbose:$False -Force
-        $UpdateInfo | Start-InstallerDownload -Verbose:$VerbosePreferenceBool
-        Remove-InstallerOutdated -UseSignature -Verbose:$VerbosePreferenceBool
-        Expand-ChromiumInstaller (Get-InstallerPath) $NameLocation -Verbose:$VerbosePreferenceBool
-        Set-ChromiumShortcut $NameLocation
-        Set-BatchRedirect 'tor' $NameLocation
-        If (!(Test-InstallOutdated -CompareInstalls)) { Write-Verbose "Tor $(Get-ExecutableVersion) installation complete." }
-    } 
+        $UpdateModule =
+            Import-CommonScript chrome-installer |
+            Import-Module -PassThru -Force -Verbose:$False
+        @{
+            UpdateInfo = $(
+                Write-Verbose 'Retrieve install or update information...'
+                Try {
+                    Get-DownloadInfo -PropertyList @{
+                        OSArch = Get-ExecutableType $NameLocation
+                    } -From TorProject | Select-NonEmptyObject
+                }
+                Catch { }
+            )
+            NameLocation = $NameLocation
+            SaveTo = $SaveTo
+            SoftwareName = 'Tor'
+            InstallerDescription = 'CN="The Tor Project, Inc."'
+            BatchRedirectName = 'tor'
+            UseTimestamp = $True
+            TimestampType = 'SigningTime'
+            UseSignature = $True
+            Verbose = $VerbosePreference -ine 'SilentlyContinue'
+        } | ForEach-Object { Invoke-CommonScript @_ }
+    }
     Catch { }
+    Finally { $UpdateModule | Remove-Module -Verbose:$False }
 }
 
 <#
