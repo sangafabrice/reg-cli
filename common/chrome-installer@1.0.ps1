c989075eeb570c8c3975904f71ab3893ccbe0522
Fabrice Sanga <69244030+sangafabrice@users.noreply.github.com>
2022-09-03 15:21:38 +0200

    increment chrome-installer script version

---
+++ common/chrome-installer@1.0.ps1
@@ -0,0 +1,84 @@
+[CmdletBinding()]
+Param (
+    [AllowNull()]
+    [pscustomobject] $UpdateInfo,
+    [string] $NameLocation,
+    [string] $SaveTo,
+    [string] $SoftwareName,
+    [string] $InstallerDescription,
+    [string] $BatchRedirectName,
+    [ValidateScript({ ForEach ($key in @('BaseNameLocation','HexColor')) { $_.ContainsKey($key) } })]
+    [hashtable] $VisualElementManifest,
+    [switch] $SkipSslValidation,
+    [switch] $UseTimestamp,
+    [string] $Checksum,
+    [string] $Extension = '.exe'
+)
+
+DynamicParam {
+    If ($UseTimestamp) {
+        $AttributeCollection = [System.Collections.ObjectModel.Collection[System.Attribute]]::New()
+        $AttributeCollection.Add([System.Management.Automation.ParameterAttribute] @{ Mandatory = $False })
+        $AttributeCollection.Add([System.Management.Automation.ValidateSetAttribute]::New('DateTime','SigningTime'))
+        $ParamDictionary = [System.Management.Automation.RuntimeDefinedParameterDictionary]::New()
+        $ParamDictionary.Add('TimestampType',[System.Management.Automation.RuntimeDefinedParameter]::New('TimestampType',[string],$AttributeCollection))
+        $PSBoundParameters.TimestampType = 'DateTime'
+        $ParamDictionary
+    }
+}
+Process {
+    $IsVerbose = $VerbosePreference -ine 'SilentlyContinue'
+    $InstallerVersion =
+        Try {
+            $UpdateInfo.LastModified ?? ([version] $UpdateInfo.Version)
+        } Catch { "$($UpdateInfo.Version)" }
+    If (!$UpdateInfo) {
+        $InstallerVersion = $(
+            $InfoArguments = @{
+                Path = $SaveTo
+                Description = $InstallerDescription
+            }
+            If ($UseTimestamp) {
+                Switch ($PSBoundParameters.TimestampType) {
+                    'DateTime'    { Get-SavedInstallerLastModified @InfoArguments }
+                    'SigningTime' { Get-SavedInstallerSigningTime @InfoArguments }
+                }
+            }
+            Else { Get-SavedInstallerVersion @InfoArguments }
+        )
+    }
+    Try {
+        Switch ($NameLocation) {
+            Default {
+                $UpdateModule =
+                    @{
+                        Path = $_
+                        SaveTo = $SaveTo
+                        Version = $InstallerVersion
+                        Description = $InstallerDescription
+                        UseSigningTime = !$UpdateInfo -and $PSBoundParameters.TimestampType -ieq 'SigningTime'
+                        Checksum = $(
+                            If ($PSBoundParameters.ContainsKey('Checksum')) { $Checksum = $Null }
+                            $Checksum
+                        )
+                        SoftwareName = $SoftwareName
+                        Extension = $Extension
+                    } | ForEach-Object { New-RegCliUpdate @_ } |
+                    Import-Module -Verbose:$False -Force -PassThru
+                $UpdateInfo.Where({ $_ }) | Start-InstallerDownload -Verbose:$IsVerbose -Force:$SkipSslValidation
+                Remove-InstallerOutdated -Verbose:$IsVerbose
+                Expand-ChromiumInstaller (Get-InstallerPath) $_ -Verbose:$IsVerbose
+                Set-ChromiumShortcut $_
+                Set-BatchRedirect $BatchRedirectName $_
+            }
+        }
+        $VisualElementManifest.Where({ $_ }) |
+        ForEach-Object { Set-ChromiumVisualElementsManifest "$($_.BaseNameLocation).VisualElementsManifest.xml" $_.HexColor }
+        If (!(Test-InstallOutdated -CompareInstalls:$UseTimestamp)) {
+            Write-Verbose "$SoftwareName $(Get-ExecutableVersion) installation complete."
+        }
+    }
+    Catch { $_ }
+    Finally { Remove-Module $UpdateModule -Verbose:$False }
+}
+End { }
