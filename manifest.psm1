539f8e0a648c0f451aaa1b1a8ff7ba4c5bd6bdf2
Fabrice Sanga <69244030+sangafabrice@users.noreply.github.com>
2022-09-05 13:24:10 +0200

    update regcli version to 6.1.1

--- manifest.psm1
+++ manifest.psm1
@@ -29,7 +29,7 @@ Function New-UpdaterScript {
                 ModuleVersion = '5.0.2'
             },@{
                 ModuleName = 'RegCli'
-                ModuleVersion = '6.0.3'
+                ModuleVersion = '6.1.1'
             })
             ExternalModuleDependencies = @('DownloadInfo','RegCli')
             Tags = $DevDependencies.Tags
