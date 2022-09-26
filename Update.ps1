9aa7784603a92000ceb8282c1b7c0c1193ad277d
Fabrice Sanga <69244030+sangafabrice@users.noreply.github.com>
2022-09-26 09:04:30 +0200

    add common script to updater

--- Update.ps1
+++ Update.ps1
@@ -11,42 +11,26 @@ Param (
 )
 
 & {
-    $NameLocation = "$InstallLocation\TuneIn.exe"
-    $VerbosePreferenceBool = $VerbosePreference -ine 'SilentlyContinue'
-    Write-Verbose 'Retrieve install or update information...'
-    $UpdateInfo = 
-        @{
-            Uri = 'https://tunein.com/download/windows/'
-            Method  = 'HEAD'
-            MaximumRedirection = 0
-            SkipHttpErrorCheck = $True
-            ErrorAction = 'SilentlyContinue'
-            Verbose = $False
-        } | ForEach-Object { (Invoke-WebRequest @_).Headers.Location } |
-        Select-Object @{
-            Name = 'Version'
-            Expression = {
-                [void] ($_ -match '(?<Version>(\d+\.)+\d+)\.exe$')
-                [version] $Matches.Version
-            }
-        },@{
-            Name = 'Link'
-            Expression = { $_ }
-        } | Select-NonEmptyObject
-    $InstallerVersion = $UpdateInfo.Version
-    $InstallerDescription = 'TuneIn Desktop app - an electron wrapper for tunein.com'
-    If (!$UpdateInfo) { $InstallerVersion = Get-SavedInstallerVersion $SaveTo $InstallerDescription }
     Try {
-        New-RegCliUpdate $NameLocation $SaveTo $InstallerVersion $InstallerDescription |
-        Import-Module -Verbose:$False -Force
-        $UpdateInfo | Start-InstallerDownload -Verbose:$VerbosePreferenceBool
-        Remove-InstallerOutdated -Verbose:$VerbosePreferenceBool
-        Expand-NsisInstaller (Get-InstallerPath) $NameLocation -Verbose:$VerbosePreferenceBool
-        Set-NsisShortcut $NameLocation
-        Set-BatchRedirect 'TuneIn' $NameLocation
-        If (!(Test-InstallOutdated)) { Write-Verbose "TuneIn $(Get-InstallerVersion) installation complete." }
-    } 
+        $UpdateModule =
+            Import-CommonScript chrome-installer |
+            Import-Module -PassThru -Force -Verbose:$False
+        @{
+            UpdateInfo = $(
+                Write-Verbose 'Retrieve install or update information...'
+                Try { Get-DownloadInfo -From TuneIn }
+                Catch { }
+            )
+            NameLocation = "$InstallLocation\TuneIn.exe"
+            SaveTo = $SaveTo
+            SoftwareName = 'TuneIn'
+            InstallerDescription = 'TuneIn Desktop app - an electron wrapper for tunein.com'
+            InstallerType = 'NSIS'
+            Verbose = $VerbosePreference -ine 'SilentlyContinue'
+        } | ForEach-Object { Invoke-CommonScript @_ }
+    }
     Catch { }
+    Finally { $UpdateModule | Remove-Module -Verbose:$False }
 }
 
 <#
@@ -82,7 +66,7 @@ Param (
     PS > Get-ChildItem | Select-Object Name
     Name
     ----
-    1.24.0.exe
+    tunein_1.24.0.exe
     UpdateTuneIn.ps1
 
     Install TuneIn to 'C:\ProgramData\TuneIn' and save its setup installer to the current directory.
