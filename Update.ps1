0c96cb9a21a53d650c8f177c902e38ca6c020267
Fabrice Sanga <69244030+sangafabrice@users.noreply.github.com>
2022-09-28 11:58:27 +0200

    add common script to updater

--- Update.ps1
+++ Update.ps1
@@ -11,39 +11,27 @@ Param (
 )
 
 & {
-    $NameLocation = "$InstallLocation\Local.exe"
-    $VerbosePreferenceBool = $VerbosePreference -ine 'SilentlyContinue'
-    Write-Verbose 'Retrieve install or update information...'
-    $UpdateInfo = 
-        @{
-            Uri = 'https://cdn.localwp.com/stable/latest/windows'
-            Method  = 'HEAD'
-            MaximumRedirection = 0
-            SkipHttpErrorCheck = $True
-            ErrorAction = 'SilentlyContinue'
-            Verbose = $False
-        } | ForEach-Object { (Invoke-WebRequest @_).Headers.Location } |
-        Select-Object @{
-            Name = 'Version'
-            Expression = { [version] (([uri] $_).Segments?[-2] -split '\+')?[0] }
-        },@{
-            Name = 'Link'
-            Expression = { $_ }
-        } | Select-NonEmptyObject
-    $InstallerVersion = $UpdateInfo.Version
-    $InstallerDescription = 'Create local WordPress sites with ease.'
-    If (!$UpdateInfo) { $InstallerVersion = Get-SavedInstallerVersion $SaveTo $InstallerDescription }
     Try {
-        New-RegCliUpdate $NameLocation $SaveTo $InstallerVersion $InstallerDescription |
-        Import-Module -Verbose:$False -Force
-        $UpdateInfo | Start-InstallerDownload -Verbose:$VerbosePreferenceBool
-        Remove-InstallerOutdated -Verbose:$VerbosePreferenceBool
-        Expand-NsisInstaller (Get-InstallerPath) $NameLocation 32 -Verbose:$VerbosePreferenceBool
-        Set-NsisShortcut $NameLocation
-        Set-BatchRedirect 'local' $NameLocation
-        If (!(Test-InstallOutdated)) { Write-Verbose "Local $(Get-InstallerVersion) installation complete." }
-    } 
+        $UpdateModule =
+            Import-CommonScript chrome-installer |
+            Import-Module -PassThru -Force -Verbose:$False
+        @{
+            UpdateInfo = $(
+                Write-Verbose 'Retrieve install or update information...'
+                Try { Get-DownloadInfo -From Local }
+                Catch { }
+            )
+            NameLocation = "$InstallLocation\Local.exe"
+            SaveTo = $SaveTo
+            SoftwareName = 'Local'
+            InstallerDescription = 'Create local WordPress sites with ease.'
+            InstallerType = 'NSIS'
+            NsisType = 32
+            Verbose = $VerbosePreference -ine 'SilentlyContinue'
+        } | ForEach-Object { Invoke-CommonScript @_ }
+    }
     Catch { }
+    Finally { $UpdateModule | Remove-Module -Verbose:$False }
 }
 
 <#
@@ -79,7 +67,7 @@ Param (
     PS > Get-ChildItem | Select-Object Name
     Name
     ----
-    6.4.2.exe
+    local_6.4.3.exe
     UpdateLocal.ps1
 
     Install Local to 'C:\ProgramData\Local' and save its setup installer to the current directory.
