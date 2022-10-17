d6ef99edba1991d71f9170d871066f7baccf041a
Fabrice Sanga <69244030+sangafabrice@users.noreply.github.com>
2022-10-17 13:26:00 +0200

    initialize filezilla client updater script.

--- Update.ps1
+++ Update.ps1
@@ -3,7 +3,7 @@ Param (
     [ValidateNotNullOrEmpty()]
     [ValidateScript({ Test-InstallLocation $_ $PSScriptRoot })]
     [string]
-    $InstallLocation = "${Env:ProgramData}\TeamViewer",
+    $InstallLocation = "${Env:ProgramData}\FileZillaClient",
     [ValidateNotNullOrEmpty()]
     [ValidateScript({ Test-InstallerLocation $_ })]
     [string]
@@ -11,7 +11,7 @@ Param (
 )
 
 & {
-    $NameLocation = "$InstallLocation\TeamViewer.exe"
+    $NameLocation = "$InstallLocation\filezilla.exe"
     Try {
         $UpdateModule =
             Import-CommonScript chrome-installer |
@@ -21,18 +21,15 @@ Param (
                 Write-Verbose 'Retrieve install or update information...'
                 Try {
                     Get-DownloadInfo -PropertyList @{
+                        Type = 'client'
                         OSArch = Get-ExecutableType $NameLocation
-                    } -From TeamViewer 
+                    } -From FileZilla 
                 }
                 Catch { }
             )
-            InstallLocation = $InstallLocation
             NameLocation = $NameLocation
             SaveTo = $SaveTo
-            SoftwareName = 'TeamViewer'
-            InstallerDescription = 'CN=TeamViewer Germany GmbH'
-            InstallerType = 'BasicNSIS'
-            CompareInstalls = $True
+            SoftwareName = 'FileZilla FTP Client'
             Verbose = $VerbosePreference -ine 'SilentlyContinue'
         } | ForEach-Object { Invoke-CommonScript @_ }
     }
@@ -42,39 +39,39 @@ Param (
 
 <#
 .SYNOPSIS
-    Updates TeamViewer software.
+    Updates FileZilla FTP Client software.
 .DESCRIPTION
-    The script installs or updates TeamViewer on Windows.
+    The script installs or updates FileZilla FTP Client on Windows.
 .NOTES
     Required: at least Powershell Core 7.
 .PARAMETER InstallLocation
     Path to the installation directory.
     It is restricted to file system paths.
     It does not necessary exists.
-    It defaults to "%ProgramData%\TeamViewer".
+    It defaults to "%ProgramData%\FileZillaClient".
 .PARAMETER SaveTo
     Path to the directory of the downloaded installer.
     It is an existing file system path.
     It defaults to the script directory.
 .EXAMPLE
-    Get-ChildItem 'C:\ProgramData\TeamViewer' -ErrorAction SilentlyContinue
+    Get-ChildItem 'C:\ProgramData\FileZillaClient' -ErrorAction SilentlyContinue
 
-    PS > .\UpdateTeamViewer.ps1 -InstallLocation 'C:\ProgramData\TeamViewer' -SaveTo .
+    PS > .\UpdateFileZillaClient.ps1 -InstallLocation 'C:\ProgramData\FileZillaClient' -SaveTo .
 
-    PS > Get-ChildItem 'C:\ProgramData\TeamViewer' | Where-Object Name -Like 'TeamViewer*' | Select-Object Name -First 5
+    PS > Get-ChildItem 'C:\ProgramData\FileZillaClient' | Select-Object Name -First 5
     Name
     ----
-    TeamViewer_Desktop.exe
-    TeamViewer_Note.exe
-    TeamViewer_Resource_ar.dll
-    TeamViewer_Resource_bg.dll
-    TeamViewer_Resource_cs.dll
+    docs
+    locales
+    resources
+    AUTHORS
+    filezilla.exe
 
     PS > Get-ChildItem | Select-Object Name
     Name
     ----
-    teamviewer_15.34.4.exe
-    UpdateTeamViewer.ps1
+    filezilla_ftp_client_3.61.0.exe
+    UpdateFileZillaClient.ps1
 
-    Install TeamViewer to 'C:\ProgramData\TeamViewer' and save its setup installer to the current directory.
+    Install FileZilla FTP Client to 'C:\ProgramData\FileZillaClient' and save its setup installer to the current directory.
 #>
