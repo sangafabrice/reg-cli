ceea0a6e97daff7ee1d85feb75d1f6d5617ef5ae
Fabrice Sanga <69244030+sangafabrice@users.noreply.github.com>
2022-10-17 12:21:05 +0200

    initialize teamviewer updater script.

--- Update.ps1
+++ Update.ps1
@@ -3,7 +3,7 @@ Param (
     [ValidateNotNullOrEmpty()]
     [ValidateScript({ Test-InstallLocation $_ $PSScriptRoot })]
     [string]
-    $InstallLocation = "${Env:ProgramData}\Wireshark",
+    $InstallLocation = "${Env:ProgramData}\TeamViewer",
     [ValidateNotNullOrEmpty()]
     [ValidateScript({ Test-InstallerLocation $_ })]
     [string]
@@ -11,8 +11,7 @@ Param (
 )
 
 & {
-    $NameLocation = "$InstallLocation\Wireshark.exe"
-    $MachineType = Get-ExecutableType $NameLocation
+    $NameLocation = "$InstallLocation\TeamViewer.exe"
     Try {
         $UpdateModule =
             Import-CommonScript chrome-installer |
@@ -20,45 +19,49 @@ Param (
         @{
             UpdateInfo = $(
                 Write-Verbose 'Retrieve install or update information...'
-                Try { Get-DownloadInfo -PropertyList @{ OSArch = $MachineType } -From Wireshark }
+                Try {
+                    Get-DownloadInfo -PropertyList @{
+                        OSArch = Get-ExecutableType $NameLocation
+                    } -From TeamViewer 
+                }
                 Catch { }
             )
             InstallLocation = $InstallLocation
             NameLocation = $NameLocation
             SaveTo = $SaveTo
-            SoftwareName = 'Wireshark'
-            InstallerDescription = "Wireshark installer for $($MachineType -replace 'x' -replace '86','32')-bit Windows"
+            SoftwareName = 'TeamViewer'
+            InstallerDescription = 'CN=TeamViewer Germany GmbH'
             InstallerType = 'BasicNSIS'
             CompareInstalls = $True
             Verbose = $VerbosePreference -ine 'SilentlyContinue'
         } | ForEach-Object { Invoke-CommonScript @_ }
     }
-    Catch { }
+    Catch { $_ }
     Finally { $UpdateModule | Remove-Module -Verbose:$False }
 }
 
 <#
 .SYNOPSIS
-    Updates Wireshark software.
+    Updates TeamViewer software.
 .DESCRIPTION
-    The script installs or updates Wireshark on Windows.
+    The script installs or updates TeamViewer on Windows.
 .NOTES
     Required: at least Powershell Core 7.
 .PARAMETER InstallLocation
     Path to the installation directory.
     It is restricted to file system paths.
     It does not necessary exists.
-    It defaults to "%ProgramData%\Wireshark".
+    It defaults to "%ProgramData%\TeamViewer".
 .PARAMETER SaveTo
     Path to the directory of the downloaded installer.
     It is an existing file system path.
     It defaults to the script directory.
 .EXAMPLE
-    Get-ChildItem 'C:\ProgramData\Wireshark' -ErrorAction SilentlyContinue
+    Get-ChildItem 'C:\ProgramData\TeamViewer' -ErrorAction SilentlyContinue
 
-    PS > .\UpdateWireshark.ps1 -InstallLocation 'C:\ProgramData\Wireshark' -SaveTo .
+    PS > .\UpdateTeamViewer.ps1 -InstallLocation 'C:\ProgramData\TeamViewer' -SaveTo .
 
-    PS > Get-ChildItem 'C:\ProgramData\Wireshark' | Select-Object Name -First 5
+    PS > Get-ChildItem 'C:\ProgramData\TeamViewer' | Select-Object Name -First 5
     Name
     ----
     diameter
@@ -70,8 +73,8 @@ Param (
     PS > Get-ChildItem | Select-Object Name
     Name
     ----
-    UpdateWireshark.ps1
-    wireshark_4.0.0.exe
+    UpdateTeamViewer.ps1
+    TeamViewer_4.0.0.exe
 
-    Install Wireshark to 'C:\ProgramData\Wireshark' and save its setup installer to the current directory.
+    Install TeamViewer to 'C:\ProgramData\TeamViewer' and save its setup installer to the current directory.
 #>
